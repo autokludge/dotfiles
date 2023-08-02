413941ef385032627d2e5f3e8e74184452f5ca69
Jesse Robertson <jessrobe@gmail.com>
2023-08-02 21:59:35 +1000

    wezterm added initial size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,17 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.initial_rows = 40
+config.initial_cols = 140
+
 config.color_scheme = "Radium (base46)"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.90
 
-config.font = wezterm.font_with_fallback {
-  'CaskaydiaCove Nerd Font', 'Noto Color Emoji'
-}
+config.font = wezterm.font_with_fallback({
+	"CaskaydiaCove Nerd Font",
+	"Noto Color Emoji",
+})
+config.font_size = 10.0
 
 return config
